20014fa81a12b1b84597ffec5975c7afcf7b6621
Vlad Dubov <dubov.web@gmail.com>
2017-12-11 00:36:32 +0300

    Активный пунк в меню

--- frontend/blocks/menu/index.js
+++ frontend/blocks/menu/index.js
@@ -1,6 +1,7 @@
 import './style.styl';
 import $ from 'jquery';
 import { TimelineMax, Power2 } from 'gsap';
+import Barba from 'barba.js';
 
 if (!window.domModules) {
   window.domModules = {};
@@ -10,8 +11,20 @@ window.domModules['menu'] = {
   init: function(el) {
     this.el = el;
     this.entryAnimation();
+
+    Barba.Dispatcher.on('newPageReady', this.onPageChange.bind(this));
+    this.onPageChange({
+      url: window.location.href
+    })
   },
 
+  onPageChange: function(currentStatus) {
+    const link = currentStatus.url.split(window.location.origin)[1].substring(1);
+    const navigationLinks = this.el.find('.menu__link');
+    const navigationLinkIsActive = this.el.find(`[href="${link}"]`);
+    navigationLinks.removeClass('active');
+    navigationLinkIsActive.addClass('active');
+  },
   entryAnimation: function() {
     const tl = new TimelineMax();
     const items = this.el.find('.menu__item');
@@ -19,3 +32,4 @@ window.domModules['menu'] = {
     tl.staggerFrom(items, 0.5, { autoAlpha: 0, y: -10, ease: Power2.easeInOut }, 0.3);
   }
 };
+
