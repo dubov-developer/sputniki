00a679a092fce49e7964414d27d4d86a04f2599b
Vlad Dubov <dubov.web@gmail.com>
2017-12-20 23:47:07 +0300

    Поправил фиксацию меню

--- frontend/blocks/case-select/index.js
+++ frontend/blocks/case-select/index.js
@@ -10,29 +10,28 @@ if (!window.domModules) {
 window.domModules['case-select'] = {
   isStucked: false,
   init: function(el) {
-    this.el = el;
-    this.wrapperOffset = this.el.parent().offset();
-    this.menu = $('.menu')
-    this.selectText = el.find('.action-line__options-text');
-    this.selectTextOffset = this.selectText.offset();
-    this.menuOffset = this.menu.offset();
-
-    this.onScroll = this.onScroll.bind(this);
-
     setTimeout(() => {
+      this.el = el;
+      this.wrapperOffset = this.el.parent().offset();
+      this.menu = $('.menu')
+      this.selectText = el.find('.action-line__options-text');
+      this.selectTextOffset = this.selectText.offset();
+      this.menuOffset = this.menu.offset();
+  
+      this.onScroll = this.onScroll.bind(this);
       scrollbar.addListener(this.onScroll);
-    })
+    }, 0)
   },
   onScroll(event) {
     if (!this.isStucked) {
-      if (this.wrapperOffset.top - this.menuOffset.top <= event.offset.y) {
+      if (this.selectTextOffset.top - this.menuOffset.top <= event.offset.y) {
         console.log('STUCK!');
         this.isStucked = true;
         this.el.addClass('stucked');
         this.el.css({ position: 'absolute' });
       }
     } else {
-      if (this.wrapperOffset.top - this.menuOffset.top > event.offset.y) {
+      if (this.selectTextOffset.top - this.menuOffset.top > event.offset.y) {
         console.log('UNSTUCK!');
         this.isStucked = false;
         this.el.removeClass('stucked');
@@ -41,7 +40,7 @@ window.domModules['case-select'] = {
     }
 
     if (this.isStucked) {
-      this.el.css({ transform: `translateY(${event.offset.y + this.menuOffset.top - this.wrapperOffset.top - 3}px)` });
+      this.el.css({ transform: `translateY(${event.offset.y + this.menuOffset.top - this.selectTextOffset.top}px)` });
     }
   },
   initTimeline(el) {
