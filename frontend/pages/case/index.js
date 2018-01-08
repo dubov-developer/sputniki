c44140aaa5daaf96bd92a0b60b21d3328858c040
Vlad Dubov <dubov.web@gmail.com>
2018-01-08 21:30:38 +0300

    Анимация линии

--- frontend/pages/case/index.js
+++ frontend/pages/case/index.js
@@ -6,7 +6,7 @@ import { scrollbar, disableScroll, enableScroll } from '../../scroll';
 import { scrollmagic } from '../../js/scrollmagic.js';
 import { willChange } from '../../js/gsap-helpers';
 
-import { TimelineLite, Power2 } from 'gsap';
+import { TimelineLite, Power2, Linear } from 'gsap';
 
 let scenes = [];
 
@@ -17,6 +17,21 @@ var Casepage = Barba.BaseView.extend({
   onEnterCompleted: function() {
     CaseEnterAnimation().then(() => {
       enableScroll();
+    });
+
+    setTimeout(() => {
+      const tl = new TimelineLite();
+      const tlWch = willChange(tl);
+      const separator = $('.case-info__separator');
+      tlWch.to(separator, 0.5, { startAt: { scaleY: 0, transformOrigin: '50% 0%' }, immediateRender: true, scaleY: 1, ease: Linear.easeNone }, 0);
+  
+      let scene = scrollmagic.scene({
+        triggerElement: $('.case-info')[0],
+        triggerHook: 0.8,
+        duration: $('.case-info').height(),
+      }, tl);
+  
+      scenes.push(scene);
     });
   },
   onLeave: function() {
