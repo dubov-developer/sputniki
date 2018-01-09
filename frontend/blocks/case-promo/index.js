f8eda78c898ec25f5c96da3cb8164fae54073071
Vlad Dubov <dubov.web@gmail.com>
2018-01-09 23:54:11 +0300

    Отображение кнопки закрыть в видео + анимация и лоудер

--- frontend/blocks/case-promo/index.js
+++ frontend/blocks/case-promo/index.js
@@ -12,44 +12,59 @@ if (!window.domModules) {
 }
 
 window.domModules['case-promo'] = {
+  hasActiveVideo: false,
   init(el) {
     el.on('click', () => {
-      this.createVideo();
+      if (!this.hasActiveVideo) {
+        this.createVideo();
+        this.hasActiveVideo = true;
+      }
     });
   },
   createVideo() {
+    this.closeVisible = false;
     this.body = $('body');
     this.wrapper = $('<div>');
-    this.wrapper.addClass('.player-wrapper');
+    this.wrapper.addClass('player-wrapper');
     const playerEl = $('<div>');
-    playerEl.addClass('.player');
+    playerEl.addClass('player');
     playerEl.attr('id', 'player');
 
-    const playerOverlay = $('<div>');
-    playerOverlay.addClass('.player-overlay');
+    this.playerOverlay = $('<div>');
+    this.playerOverlay.addClass('player-overlay');
 
-    this.wrapper.css({
-      'position': 'fixed',
-      'top': 0,
-      'left': 0,
-      'width': '100%',
-      'height': '100%',
-      'background-color': '#000'
-    });
+    this.playerClose = $('<div>');
+    this.playerClose.addClass('player-close');
 
-    playerOverlay.css({
-      'position': 'absolute',
-      'top': 0,
-      'left': 0,
-      'width': '100%',
-      'height': '100%',
-    });
-
+    this.playerOverlay.append(this.playerClose);
     this.wrapper.append(playerEl);
-    this.wrapper.append(playerOverlay);
+    this.wrapper.append(this.playerOverlay);
     this.body.append(this.wrapper);
+    this.closeSize = this.playerClose.height() / 2;
 
-    playerOverlay.on('click', () => {
+    TweenMax.set(this.wrapper, { autoAlpha: 0, scale: 0.5 });
+
+    this.playerOverlay.on('mouseleave', (e) => {
+      this.closeVisible = false;
+      TweenMax.to(this.playerClose, 0.5, { scale: 0, ease: Power2.easeOut });
+    });
+
+    this.playerOverlay.on('mousemove', (e) => {
+      const top = e.pageY - this.closeSize;
+      const left = e.pageX - this.closeSize;
+
+      this.playerClose.css({
+        top,
+        left
+      });
+
+      if (!this.closeVisible) {
+        TweenMax.to(this.playerClose, 0.5, { scale: 1, ease: Power2.easeOut });
+        this.closeVisible = true;
+      }
+    });
+
+    this.playerOverlay.on('click', () => {
       this.removeVideo();
     });
 
@@ -66,10 +81,25 @@ window.domModules['case-promo'] = {
     });
 
     player.load('v8HxC9mr1vE');
+
+    window['domModules'].loader.start();
+
+    player.on('playing', () => {
+      window['domModules'].loader.done();
+      TweenMax.to(this.wrapper, 0.8, { autoAlpha: 1, scale: 1, ease: Power2.easeInOut });
+    });
+
+    player.on('ended', () => {
+      this.removeVideo();
+    })
+
     disableScroll();
   },
   removeVideo() {
-    enableScroll();
-    this.wrapper.remove();
+    TweenMax.to(this.wrapper, 0.5, { autoAlpha: 0, scale: 0.5, ease: Power2.easeInOut, onComplete: () => {
+      enableScroll();
+      this.hasActiveVideo = false;
+      this.wrapper.remove();
+    }});
   }
 };
