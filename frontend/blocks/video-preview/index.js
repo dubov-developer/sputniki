3d387d73664398b12f9ec2fccc30bc3e0d1a096f
Vlad Dubov <dubov.web@gmail.com>
2018-02-11 15:15:34 +0300

    +

--- frontend/blocks/video-preview/index.js
+++ frontend/blocks/video-preview/index.js
@@ -1,6 +1,7 @@
 import $ from 'jquery';
 import { TimelineMax, TweenMax, Power2 } from 'gsap';
 import getViewport from 'getviewport';
+import { scrollbar, disableScroll, enableScroll } from '../../scroll.js';
 
 const videoUrls = {
   'di-caprio': require('../../video/di-caprio.mp4'),
@@ -21,21 +22,27 @@ if (!window.domModules) {
 
 window.domModules['video-preview'] = {
   init(el) {
-    console.log('HERE', el);
+    this.prevMousePosition = null;
     this.el = el;
     this.prevCase = null;
     this.firstContainer = this.el.find('.preview-case__canvas-container').eq(0);
     this.videoRatio = 960 / 540;
     this.background = this.el.find('.preview-case__background');
     this.onPageTransitionCompleted = this.onPageTransitionCompleted.bind(this);
-    this.onWindowResize = this.onWindowResize.bind(this)
-
+    this.onWindowResize = this.onWindowResize.bind(this);
+    this.onScroll = this.onScroll.bind(this)
+    this.onDocumentMouseMove = this.onDocumentMouseMove.bind(this);
     window.addEventListener('resize', this.onWindowResize);
     this.onWindowResize();
     $('body').one('pageTransitionCompleted', this.onPageTransitionCompleted);
+    $(document).on('mousemove', this.onDocumentMouseMove);
+    setTimeout(() => {
+      scrollbar.addListener(this.onScroll);
+    });
 
     this.el.on('mouseenter', '.preview-case', (e) => {
       if (!this.inited) {
+        this.inited = true;
         this.caseEl = $(e.target).closest('.preview-case');
         this.container = this.caseEl.find('.preview-case__canvas-container');
         this.caseName = this.caseEl.data('case');
@@ -47,13 +54,32 @@ window.domModules['video-preview'] = {
       this.destroyVideo();
     });
   },
+  onDocumentMouseMove(e) {
+    this.prevMousePosition = {
+      x: e.clientX,
+      y: e.clientY
+    };
+    this.prevScrollOffset = scrollbar.offset.y;
+  },
+  onScroll(e) {
+    if (this.prevMousePosition) {
+      const elem = document.elementFromPoint(this.prevMousePosition.x, this.prevMousePosition.y);
+      const caseName = $(elem).closest('.preview-case').data('case');
+  
+      if (this.caseName !== caseName) {
+        $(`.preview-case[data-case="${this.caseName}"]`).trigger('mouseleave');
+        $(`.preview-case[data-case="${caseName}"]`).trigger('mouseenter');
+      }
+    }
+  },
   onPageTransitionCompleted() {
     this.destroyVideo();
     window.removeEventListener('resize', this.onWindowResize);
+    $(document).off('mousemove', this.onDocumentMouseMove);
+    scrollbar.removeListener(this.onScroll);
   },
   createVideo() {
     this.loadVideo(videoUrls[this.caseName]).then(() => {
-      this.inited = true;
       this.container.append('<canvas class="preview-case__canvas"></canvas>')
       this.canvas = this.container.find('canvas')[0];
       this.ctx = this.canvas.getContext('2d');
@@ -70,17 +96,24 @@ window.domModules['video-preview'] = {
   },
   destroyVideo() {
     if (this.inited) {
+      this.caseName = null;
       clearInterval(this.currentTimer);
-      this.ctx.clearRect(0, 0, this.width, this.height);
-      this.canvas.remove();
-      this.canvas = null;
+      if (this.ctx) {
+        this.ctx.clearRect(0, 0, this.width, this.height);
+      }
+      if (this.canvas) {
+        this.canvas.remove();
+        this.canvas = null;
+      }
       this.ctx = null;
       $(this.videoElement).remove();
       this.inited = false;
     }
   },
   renderFrame(video) {
-    this.ctx.drawImage(video, 0, 0, 960, 540, this.fitSizes.x, this.fitSizes.y, this.fitSizes.width, this.fitSizes.height);
+    if (this.ctx) {
+      this.ctx.drawImage(video, 0, 0, 960, 540, this.fitSizes.x, this.fitSizes.y, this.fitSizes.width, this.fitSizes.height);
+    }
   },
   loadVideo(url) {
     return new Promise((resolve, reject) => {
